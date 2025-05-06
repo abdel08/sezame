1ade53c001396e700fb0514531b9c338d3f80fc0
unknown <abdel.hka7@gmail.com>
2025-05-06 08:55:58 +0100

    fix: disable prerender on calendar page

--- src/app/admin/calendar/page.tsx
+++ src/app/admin/calendar/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import type { View } from 'react-big-calendar';
 import { useEffect, useState } from 'react';
 import { Calendar, dateFnsLocalizer, EventPropGetter } from 'react-big-calendar';
